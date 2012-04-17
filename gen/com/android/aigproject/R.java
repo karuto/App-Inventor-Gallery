e125d0e146a8c45bdbed7b8398ae443c483f27b1
Vincent Z <iamkaruto@gmail.com>
2012-04-17 16:02:49 -0700

    Fixed display issues in detail activity, starting to inflate default listview and process more data

--- gen/com/android/aigproject/R.java
+++ gen/com/android/aigproject/R.java
@@ -25,50 +25,49 @@ public final class R {
         public static final int sync=0x7f020007;
     }
     public static final class id {
-        public static final int SearchAllField=0x7f07001d;
-        public static final int SearchSpecificField=0x7f07001f;
-        public static final int beforeAuthor=0x7f070009;
-        public static final int beforeNumComments=0x7f070015;
-        public static final int beforeNumDownloads=0x7f070013;
-        public static final int beforeNumLikes=0x7f07000f;
-        public static final int beforeNumViewed=0x7f070011;
-        public static final int beforeTitle=0x7f070007;
-        public static final int btnClose=0x7f070017;
-        public static final int buttonSearch=0x7f070029;
-        public static final int descBand=0x7f07000c;
-        public static final int download=0x7f07000b;
-        public static final int editText1=0x7f07001e;
-        public static final int editTextAuthorID=0x7f070027;
-        public static final int editTextDescription=0x7f070023;
-        public static final int editTextTag=0x7f070025;
-        public static final int editTextTitle=0x7f070021;
+        public static final int SearchAllField=0x7f07001c;
+        public static final int SearchSpecificField=0x7f07001e;
+        public static final int beforeAuthor=0x7f070005;
+        public static final int beforeNumComments=0x7f070011;
+        public static final int beforeNumDownloads=0x7f07000f;
+        public static final int beforeNumLikes=0x7f07000b;
+        public static final int beforeNumViewed=0x7f07000d;
+        public static final int btnClose=0x7f070013;
+        public static final int buttonSearch=0x7f070028;
+        public static final int descBand=0x7f070008;
+        public static final int download=0x7f070007;
+        public static final int editText1=0x7f07001d;
+        public static final int editTextAuthorID=0x7f070026;
+        public static final int editTextDescription=0x7f070022;
+        public static final int editTextTag=0x7f070024;
+        public static final int editTextTitle=0x7f070020;
         public static final int globalHeader=0x7f070002;
-        public static final int img=0x7f070006;
-        public static final int innergrid=0x7f07001a;
-        public static final int linearLayout1=0x7f070028;
-        public static final int listView1=0x7f07002b;
+        public static final int img=0x7f070003;
+        public static final int innergrid=0x7f070016;
+        public static final int linearLayout1=0x7f070027;
+        public static final int listView1=0x7f07002a;
         public static final int listViewCats=0x7f070000;
-        public static final int radioAll=0x7f070004;
-        public static final int radioGroup1=0x7f070003;
-        public static final int radioSpecific=0x7f070005;
-        public static final int statBand=0x7f07000e;
-        public static final int textViewAuthorId=0x7f070026;
-        public static final int textViewDescription=0x7f070022;
-        public static final int textViewTag=0x7f070024;
-        public static final int textViewTitle=0x7f070020;
-        public static final int thumbnail=0x7f070019;
+        public static final int radioAll=0x7f07001a;
+        public static final int radioGroup1=0x7f070019;
+        public static final int radioSpecific=0x7f07001b;
+        public static final int statBand=0x7f07000a;
+        public static final int textViewAuthorId=0x7f070025;
+        public static final int textViewDescription=0x7f070021;
+        public static final int textViewTag=0x7f070023;
+        public static final int textViewTitle=0x7f07001f;
+        public static final int thumbnail=0x7f070015;
         public static final int topbanner=0x7f070001;
-        public static final int txtAuthor=0x7f07000a;
-        public static final int txtByAuthor=0x7f07001b;
-        public static final int txtDesc=0x7f07000d;
-        public static final int txtHeader=0x7f070018;
-        public static final int txtLikes=0x7f07001c;
-        public static final int txtNumComments=0x7f070016;
-        public static final int txtNumDownloads=0x7f070014;
-        public static final int txtNumLikes=0x7f070010;
-        public static final int txtNumViewed=0x7f070012;
-        public static final int txtTitle=0x7f070008;
-        public static final int waiting=0x7f07002a;
+        public static final int txtAuthor=0x7f070006;
+        public static final int txtByAuthor=0x7f070017;
+        public static final int txtDesc=0x7f070009;
+        public static final int txtHeader=0x7f070014;
+        public static final int txtLikes=0x7f070018;
+        public static final int txtNumComments=0x7f070012;
+        public static final int txtNumDownloads=0x7f070010;
+        public static final int txtNumLikes=0x7f07000c;
+        public static final int txtNumViewed=0x7f07000e;
+        public static final int txtTitle=0x7f070004;
+        public static final int waiting=0x7f070029;
     }
     public static final class layout {
         public static final int another=0x7f030000;
@@ -83,12 +82,10 @@ public final class R {
         public static final int hello=0x7f050000;
     }
     public static final class style {
-        public static final int band=0x7f060002;
-        public static final int center=0x7f060006;
-        public static final int detail=0x7f060004;
-        public static final int detail_info=0x7f060005;
-        public static final int label=0x7f060003;
+        public static final int band=0x7f060001;
+        public static final int detail=0x7f060003;
+        public static final int label=0x7f060002;
         public static final int midtext=0x7f060000;
-        public static final int smalltext=0x7f060001;
+        public static final int smalltext=0x7f060004;
     }
 }
